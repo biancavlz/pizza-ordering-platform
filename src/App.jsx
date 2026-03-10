0c6d4decfcc2a41cb039bb8a59fb497cad1211b4
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-10 11:27:15 +0100

    Install react-router-dom and configurs createBrowserRouter()

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
