61532542fbb7f72d1171906e7fbc308410958ce5
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-11 14:24:41 +0100

    Handle form validation errors with react-router

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
-// const isValidPhone = (str) =>
-//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-//     str,
-//   );
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
 
 const fakeCart = [
   {
@@ -39,6 +39,8 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitted = navigation.state === "submitting";
 
+  const formErrors = useActionData();
+
   return (
     <div>
       <h2>Ready to order? Let&apos;s go!</h2>
@@ -55,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -96,7 +99,11 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
-  console.log(order);
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone = "Please, provide your correct phone number";
+
+  if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
