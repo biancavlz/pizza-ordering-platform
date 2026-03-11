f2d55ea520c597d6c75f404e27d8055c0ec7d1ee
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-11 10:28:38 +0100

    format cart and priority data

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -85,7 +85,13 @@ export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
-  console.log(data);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  console.log(order);
 
   return null;
 }
