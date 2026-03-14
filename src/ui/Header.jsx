eb9952e2cc310c9635ea2ccf0fb34d87d1e38f9a
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-14 11:46:15 +0100

    style button states

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="item-center flex justify-between border-b-2 border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="item-center flex justify-between border-b-2 border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Pizza Ordering
       </Link>
