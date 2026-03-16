6df9e240ae01ef94894ff263b0148ac81172786c
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-16 15:08:36 +0100

    Display no name when no data is provided

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 function UserName() {
   const userName = useSelector((state) => state.user.UserName);
 
+  if (!userName) return null;
+
   return (
     <div className="hidden text-sm font-semibold md:block">{userName}</div>
   );
