87eae28b3a58d0df965a1054c4eb8fcf2a718675
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-18 10:29:11 +0100

    Add + and - button and update store

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,5 +1,6 @@
 import { formatCurrency } from '../../utils/helpers';
 import DeleteButton from './DeleteItem';
+import UpdateItemQuantity from './UpdateItemQuantity';
 
 function CartItem({ item }) {
   const { name, quantity, totalPrice, pizzaId } = item;
@@ -11,6 +12,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <UpdateItemQuantity pizzaId={pizzaId} />
         <DeleteButton pizzaId={pizzaId} />
       </div>
     </li>
