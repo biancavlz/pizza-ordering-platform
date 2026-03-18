c5cb688a82ee0bcf493ee7629d35c6aca9af8eb9
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-18 10:48:02 +0100

    Add dec and inc button in menu

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
 import DeleteButton from './DeleteItem';
 import UpdateItemQuantity from './UpdateItemQuantity';
+import { getCurrentQuantityById } from './cartSlice';
 
 function CartItem({ item }) {
   const { name, quantity, totalPrice, pizzaId } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -12,8 +16,11 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <UpdateItemQuantity pizzaId={pizzaId} />
-        <DeleteButton pizzaId={pizzaId} />
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
+        <DeleteButton pizzaId={pizzaId} currentQuantity={currentQuantity} />
       </div>
     </li>
   );
