dd75ee8c1555e651945fc9031b7f609e4f113b39
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-20 17:17:23 +0100

    Fetch menu data to display in order using useFetcher() hook

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,16 +1,21 @@
 import { formatCurrency } from '../../utils/helpers';
 
-function OrderItem({ item }) {
+function OrderItem({ item, ingredients, isLoadingIngredients }) {
   const { quantity, name, totalPrice } = item;
 
+  console.log(ingredients);
+
   return (
-    <li className="py-3">
+    <li className="space-y-1 py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? 'loading...' : ingredients.join(', ')}
+      </p>
     </li>
   );
 }
